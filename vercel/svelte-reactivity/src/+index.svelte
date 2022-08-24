aa79ea98b85bcb13c5cf8c9571c1cebd266bbc06
BloodShop <koliakovcr7@gmail.com>
2022-08-24 14:33:59 +0300

    test

---
+++ vercel/svelte-reactivity/src/+index.svelte
@@ -0,0 +1,14 @@
+<script>
+  let quantity = 0;
+  function addToCart() {
+    inventory.push(quantity)
+    quantity++
+  };
+  let inventory = [1,2,3];
+</script>
+
+<h1>hi there looking good hwew ia {quantity}.</h1>
+<button on:click={addToCart}>
+  Add To Cart
+</button>
+<h2>{inventory}</h2>
